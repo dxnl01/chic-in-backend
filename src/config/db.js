88e84623499c40e9391a6218c833b1f6f3cf6a14
dxnl01 <daniel.atehortua@utp.edu.co>
2024-07-10 19:12:10 -0500

    Fix to API deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-
+/*
 const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT, 
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: console.log,
+}); */
+
+const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: "postgres",
   protocol: "postgres",
   logging: console.log,
